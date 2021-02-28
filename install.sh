9c34846f0d46cc326a1295dce7b6671de5be9683
bayualhaq <50412409+bayualhaq@users.noreply.github.com>
2021-02-28 18:28:35 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#sstidjg (Wegare)
+printf 'ctrl+c' | crontab -e > /dev/null
+opkg update && opkg install unzip
+cek=$(cat /etc/openwrt_r*)
+if [[ $cek == *"LEDE"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"aarch64_cortex-a53"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mips_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mipsel_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+else
+echo -e "version anda tidak terdeteksi!"
+exit
+fi
+cek2=$(ls /usr/bin/ | grep ss-local)
+if [ $cek2 = "ss-local" ]; then
+echo > /dev/null
+else
+opkg install shadowsocks-libev-ss-local
+fi
+wget --no-check-certificate "https://raw.githubusercontent.com/bayualhaq/sstidjg/main/sstidjg.sh" -O /usr/bin/sstidjg
+wget --no-check-certificate "https://raw.githubusercontent.com/bayualhaq/sstidjg/main/autorekonek-sstidjg.sh" -O /usr/bin/autorekonek-sstidjg
+wget --no-check-certificate "https://github.com/bayualhaq/sstidjg/blob/main/simple-obfs_0.0.5-5_aarch64_cortex-a53.ipk?raw=true" -O ~/simple-obfs.ipk
+wget --no-check-certificate "https://github.com/bayualhaq/sstidjg/blob/main/ck-client?raw=true" -O /usr/bin/ck-client
+opkg install ip-full && opkg install *.ipk && opkg install lsof && opkg install fping
+chmod +x /usr/bin/sstidjg
+chmod +x /usr/bin/autorekonek-sstidjg
+chmod +x /usr/bin/ck-client
+rm -r ~/*.ipk
+rm -r ~/install.sh
+mkdir -p ~/akun/
+touch ~/akun/sstidjg.conf
+touch ~/akun/sstidjg.txt
+sleep 2
+echo "install selesai"
+echo "untuk memulai tools silahkan jalankan perintah 'sstidjg'"
+
+				
